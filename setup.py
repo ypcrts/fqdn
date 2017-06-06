96b240dbc8e9ab05b8faa9654c0e26150e705393
ypcrts <32625281+ypcrts@users.noreply.github.com>
2017-06-06 11:16:55 -0400

    initial

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+setup(
+    name='fqdn',
+    packages=['fqdn'],
+    version='0.0.1',
+    description=('Validate fully-qualified domain names compliant '
+                 'to RFC 1035 and the preferred form in RFC 3686 s. 2.'),
+    install_requires=[],
+    author='0x6768',
+    author_email='0x6768@gmail.com',
+    url='https://github.com/0x6768/fqdn',
+    keywords=['fqdn','domain','hostname'],
+    license='MPL 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+        'Environment :: Web Environment',
+    ]
+)
