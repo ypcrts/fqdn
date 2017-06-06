2f6a90ca606740741dcc5b5500db973b8b85f89d
0x6768 <0x6768@gmail.com>
2017-06-06 11:16:55 -0400

    initial

---
+++ fqdn/__init__.py
@@ -0,0 +1,54 @@
+import re
+
+
+class FQDN:
+    FQDN_REGEX = re.compile('^((?!-)[-A-Z\d]{1,63}(?<!-)\.)+[A-Z]{1,63}\.?$',
+                            re.IGNORECASE)
+
+    def __init__(self, fqdn: str):
+        try:
+            assert fqdn and isinstance(fqdn, str)
+        except:
+            raise ValueError("fqdn must be str")
+        self.fqdn = fqdn
+        self.__is_valid = self.__validate()
+
+    def __str__(self):
+        return self.fqdn
+
+    @property 
+    def is_valid(self):
+        return self.__is_valid
+
+    def __validate(self) -> bool:
+        """
+        Validates a fully-qualified domain name (FQDN), in full compliance with
+        RFC 1035, and the "preferred form" specified in RFC 3686 s. 2.
+
+        Tolerates both relative and absolute FQDNs.
+
+        https://tools.ietf.org/html/rfc3696#section-2
+        https://tools.ietf.org/html/rfc1035
+        """
+        if len(self.fqdn) > 255:
+            return False
+        return bool(self.FQDN_REGEX.match(self.fqdn))
+
+    @property
+    def is_valid_absolute(self) -> bool:
+        """
+        Validates a fully-qualified domain name (FQDN) is RFC preferred-form
+        compliant, and ends with a `.`. With non-absolute fqdns (relative, in
+        DNS lookups, the current hosts domain name will be appended.
+        """
+        return self.fqdn.endswith('.') and self.is_valid
+
+    @property
+    def absolute(self) -> str:
+        if not self.is_valid:
+            raise ValueError(f'invalid FQDN `{self.fqdn}`')
+
+        if self.is_valid_absolute:
+            return self.fqdn
+
+        return f'{self.fqdn}.'
---
+++ tests/test_fqdn.py
@@ -0,0 +1,94 @@
+from fqdn import FQDN
+from unittest import TestCase
+
+
+class TestFQDNValidation(TestCase):
+
+    def test_constructor(self):
+        with self.assertRaises(ValueError):
+            f = FQDN(None)
+
+        with self.assertRaises(ValueError):
+            f = FQDN(b'')
+
+        with self.assertRaises(ValueError):
+            f = FQDN(b'helloworld')
+
+    def test_str(self):
+        d = 'greatdomain.com'
+        assert d == str(FQDN(d))
+        d += '.'
+        assert d == str(FQDN(d))
+
+    def test_rfc_1035_s_2_3_4__label_too_long(self):
+        self.__assert_invalid('A' * 64, 'com')
+        self.__assert_invalid('b' * 63, 'A' * 64, 'com')
+
+    def test_rfc_1035_s_2_3_4__name_too_long_256_octets(self):
+        parts = *[(chr(ord('A') + i % 26))
+                  for i in range(int(256 / 2) - 1)], 'co'
+        fqdn = '.'.join(parts)
+        assert len(fqdn) == 256
+        self.__assert_invalid(fqdn)
+
+    def test_rfc_1035_s_2_3_4__name_ok_255_octets(self):
+        parts = [(chr(ord('A') + i % 26)) for i in range(int(256 / 2))]
+        fqdn = '.'.join(parts)
+        assert len(fqdn) == 255
+        self.__assert_valid(fqdn)
+
+    def test_rfc_3696_s_2__label_invalid_starts_or_ends_with_hyphen(self):
+        self.__assert_invalid('-a', 'com')
+        self.__assert_invalid('a-', 'com')
+        self.__assert_invalid('-a-', 'com')
+        self.__assert_invalid('a', '-com')
+        self.__assert_invalid('a', 'com-')
+
+    def test_rfc_3696_s_2__preferred_form_invalid_chars(self):
+        # these should use punycode instead
+        self.__assert_invalid('є', 'com')
+        self.__assert_invalid('le-tour-est-joué', 'com')
+        self.__assert_invalid('invalid', 'cóm')
+        self.__assert_invalid('ich-hätte-gern-ein-Umlaut', 'de')
+        self.__assert_invalid('\x01', 'com')
+        self.__assert_invalid('x', '\x01\x02\x01')
+
+    def test_rfc_3696_s_2__valid(self):
+        self.__assert_valid(
+            'shopping',
+            'on',
+            'the'
+            'net')
+        self.__assert_valid('who', 'is')
+        self.__assert_valid('bbc', 'co', 'uk')
+        self.__assert_valid('example', 'io')
+
+    def __assert_invalid(self, *seq):
+        self.assertFalse(self.__is_valid_fqdn_from_labels_sequence(seq))
+
+    def __assert_valid(self, *seq):
+        self.assertTrue(self.__is_valid_fqdn_from_labels_sequence(seq))
+
+    def __is_valid_fqdn_from_labels_sequence(self, fqdn_labels_sequence):
+        fqdn = '.'.join(fqdn_labels_sequence)
+        return FQDN(fqdn).is_valid
+
+
+class TestAbsoluteFQDN(TestCase):
+    def test_absolute_fqdn(self):
+        self.assertTrue(FQDN('trainwreck.com.').is_valid_absolute)
+
+    def test_absolute_fqdn__fail(self):
+        self.assertFalse(FQDN('trainwreck.com').is_valid_absolute)
+
+    def test_to_absolute_fqdn_from_relative(self):
+        self.assertEqual(FQDN('trainwreck.com').absolute,
+                         'trainwreck.com.')
+
+    def test_to_absolute_fqdn_from_absolute(self):
+        self.assertEqual(FQDN('absolutetrainwreck.com.').absolute,
+                         'absolutetrainwreck.com.')
+
+    def test_to_absolute_fqdn__raises_ValueError(self):
+        with self.assertRaises(ValueError):
+            FQDN('trainwreckcom').absolute
