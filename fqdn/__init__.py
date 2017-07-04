d3479bf07cf7872b4cfffe31b7e56ef1510c8079
guy hughes <g@gxg.me>
2017-07-03 22:03:00 -0400

    flake8

--- fqdn/__init__.py
+++ fqdn/__init__.py
@@ -6,13 +6,13 @@ class FQDN:
     From https://tools.ietf.org/html/rfc1035#page-9,  RFC 1035 3.1. Name space
     definitions:
 
-        Domain names in messages are expressed in terms of a sequence of labels.
-        Each label is represented as a one octet length field followed by that
-        number of octets.  Since every domain name ends with the null label of
-        the root, a domain name is terminated by a length byte of zero.  The
-        high order two bits of every length octet must be zero, and the
-        remaining six bits of the length field limit the label to 63 octets or
-        less.
+        Domain names in messages are expressed in terms of a sequence of
+        labels. Each label is represented as a one octet length field followed
+        by that number of octets.  Since every domain name ends with the null
+        label of the root, a domain name is terminated by a length byte of
+        zero.  The high order two bits of every length octet must be zero, and
+        the remaining six bits of the length field limit the label to 63 octets
+        or less.
 
         To simplify implementations, the total length of a domain name (i.e.,
         label octets and label length octets) is restricted to 255 octets or
