f65d80bf01ae8a75e3caab89eacdda3ba11cda99
ypcrts <32625281+ypcrts@users.noreply.github.com>
2017-07-21 20:43:06 -0400

    cached_property, py27,34,35,36

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+tests_require = ('pytest',)
 setup(
     name='fqdn',
     packages=['fqdn'],
@@ -9,20 +10,24 @@ setup(
     author='Guy Hughes',
     author_email='0x6768@gmail.com',
     url='https://github.com/guyhughes/fqdn',
-    keywords=['fqdn', 'domain', 'hostname'],
+    keywords=['fqdn', 'domain', 'hostname', 'RFC3686', 'dns'],
     license='MPL 2.0',
     zip_safe=True,
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    setup_requires=['pytest-runner', 'cached-property>=1.3.0'],
+    tests_require=tests_require,
+    extras_require=dict(test=tests_require),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python :: 3.6',
+        'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: System :: Systems Administration',
         'Topic :: Utilities',
-        'Environment :: Web Environment',
     ]
 )
