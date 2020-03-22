440c5bd1b04bfcd2e94585040f812bdeaa5920c4
ypcrts <32625281+ypcrts@users.noreply.github.com>
2020-03-21 18:18:16 -0700

    Bump version to 1.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="fqdn",
     packages=["fqdn"],
-    version="1.3.0",
+    version="1.3.1",
     description=(
         "Validate fully-qualified domain names compliant "
         "to RFC 1035 and the preferred form in RFC 3686 s. 2."
